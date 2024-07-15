5fc495e5d79fb978029faf71af6da02b2bd4cfd2
Oliver Roldan <oproldan1@gmail.com>
2024-07-15 14:18:51 -0700

    add grid styles for artist studio

--- app/routes/artist-studio.tsx
+++ app/routes/artist-studio.tsx
@@ -7,9 +7,9 @@ export async function loader({context}: loaderfunctionargs) {
 
 export default function ArtistStudio() {
   return (
-    <div>
-      <section>Nav</section>
-      <section>Body</section>
+    <div className="grid grid-cols-8 grid-rows-8 gap-3">
+      <section className="col-start-1 col-span-1 row-start-1 row-span-8 row-end-8 bg-green-600 rounded">Nav</section>
+      <section className="row-span-2 col-span-7 bg-yellow-500 rounded">Body</section>
     </div>
   );
 }
